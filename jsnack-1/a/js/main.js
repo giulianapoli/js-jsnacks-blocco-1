e30861dff1541a991566e9da305c0c5bc2e53793
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-10 15:42:58 +0100

    Completes jsnack-1

---
+++ jsnack-1/a/js/main.js
@@ -0,0 +1,5 @@
+var palla = {
+  'nome': 'palla',
+  'peso': 10
+};
+console.log(palla);
---
+++ jsnack-1/b/js/main.js
@@ -0,0 +1,11 @@
+var palla = {
+  'nome': 'palla',
+  'peso': 10
+};
+console.log(palla);
+
+var inputUser = prompt("Inserisci il peso della palla.");
+console.log(inputUser);
+
+palla.peso = inputUser;
+console.log(palla);
